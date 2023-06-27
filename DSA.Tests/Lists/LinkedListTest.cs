d49d938b4e2312497bf8c5deca703dd74b5e76f4
Kiril Morozov <47004667+ReZerVV@users.noreply.github.com>
2023-06-27 15:58:14 +0300

    Created push back linked list test

---
+++ DSA.Tests/Lists/LinkedListTest.cs
@@ -0,0 +1,19 @@
+﻿namespace DSA.Tests.Lists
+{
+    [TestClass]
+    public class LinkedListTest
+    {
+        [TestMethod]
+        public void PushBack_LinkedList()
+        {
+            int expectedValue = 10;
+            int expectedLength = 1;
+            
+            data_structures.Lists.LinkedList<int> list = new data_structures.Lists.LinkedList<int>();
+            list.PushBack(expectedValue);
+
+            Assert.AreEqual(expectedLength, list.Length);
+            Assert.AreEqual(expectedValue, list[0]);
+        }
+    }
+}
