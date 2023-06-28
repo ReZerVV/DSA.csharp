b18591e7912a7e62c2d4de6906cadfdc4f543bee
Kiril Morozov <47004667+ReZerVV@users.noreply.github.com>
2023-06-28 13:11:14 +0300

    Created IList interface

---
+++ data_structures/Lists/IList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace data_structures.Lists
+{
+    public interface IList<T> : IEnumerable<T>, IEnumerable
+    {
+        int Length { get; }
+
+        void Add(T value);
+
+        void Clear();
+        
+        bool Contains(T value);
+        
+        int IndexOf(T value);
+        
+        void Insert(int index, T value);
+        
+        void Remove(T value);
+        
+        void RemoveAt(int index);
+
+        bool Empty();
+    }
+}
