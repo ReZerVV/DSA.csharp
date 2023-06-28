122e51bf058ea1bbd012f64cf5ee4892214ccfc8
Kiril Morozov <47004667+ReZerVV@users.noreply.github.com>
2023-06-28 15:04:00 +0300

    tests pack for insertion sort

---
+++ DSA.Tests/Algorithms/SortTests.cs
@@ -0,0 +1,15 @@
+﻿namespace DSA.Tests.Algorithms
+{
+    [TestClass]
+    public class SortTests
+    {
+        [TestMethod]
+        public void InsertionSortTest()
+        {
+            int[] expectedCollection = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] actualCollection = algorithms.Sorting.Sort<int>.Insertion(new int[]{ 4, 6, 8, 2, 1, 3, 5, 7, 9 });
+ 
+            CollectionAssert.AreEqual(expectedCollection, actualCollection);
+        }
+    }
+}
