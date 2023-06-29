b78da70f0c94dcd63b3cf5fde2e73db0058dd183
Kiril Morozov <47004667+ReZerVV@users.noreply.github.com>
2023-06-29 19:40:34 +0300

    tests pack for heap sort

--- DSA.Tests/Algorithms/SortTests.cs
+++ DSA.Tests/Algorithms/SortTests.cs
@@ -8,7 +8,16 @@
         {
             int[] expectedCollection = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             int[] actualCollection = algorithms.Sorting.Sort<int>.Insertion(new int[]{ 4, 6, 8, 2, 1, 3, 5, 7, 9 });
- 
+
+            CollectionAssert.AreEqual(expectedCollection, actualCollection);
+        }
+
+        [TestMethod]
+        public void HeapSortTest()
+        {
+            int[] expectedCollection = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] actualCollection = algorithms.Sorting.Sort<int>.HeapSort(new int[] { 4, 6, 8, 2, 1, 3, 5, 7, 9 });
+
             CollectionAssert.AreEqual(expectedCollection, actualCollection);
         }
     }
