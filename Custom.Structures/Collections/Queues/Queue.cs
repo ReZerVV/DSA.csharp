8ce585e9629a845b64685c97591a09d79c53279b
Kiril Morozov <47004667+ReZerVV@users.noreply.github.com>
2023-08-11 20:13:48 +0300

    Created queue

---
+++ Custom.Structures/Collections/Queues/Queue.cs
@@ -0,0 +1,42 @@
+﻿namespace Custom.Structures.Collections.Queues
+{
+    public class Queue<T>
+    {
+        private Lists.LinkedList<T> items;
+
+        public bool Empty()
+        {
+            return items.Empty();
+        }
+
+        public void Clear()
+        {
+            items.Clear();
+        }
+
+        public void Enqueue(T value)
+        {
+            items.Add(value);
+        }
+
+        public T Dequeue()
+        {
+            if (Empty())
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            T value = items[0];
+            items.RemoveAt(0);
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (Empty())
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return items[0];
+        }
+    }
+}
